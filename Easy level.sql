7fda6cf6ff926b8d7e724bae931fec32bc9e1de6
Sachin Chaudhary <136149331+choudhry64@users.noreply.github.com>
2025-04-30 18:33:03 +0530

    Add files via upload

---
+++ Easy level.sql
@@ -0,0 +1,40 @@
+--MODERATE QUESTION
+1 ' Write query to return the email, first name, last name, & Genre of all Rock Music listeners.
+Return your list ordered alphabetically by email starting with A.'''
+
+
+select distinct first_name, last_name, email
+from customer  
+join invoice on customer.customer_id = invoice.customer_id
+join invoice_line on invoice.invoice_id = invoice_line.invoice_id
+where track_id in(
+         select track_id from track
+         join genre on track.genre_id = genre.genre_id
+         where genre.name like 'Rock'
+)
+order by email;
+
+
+2 'Lets invite the artists who have written the most rock music in our dataset.
+   Write a query that returns the Artist name and total track count of the top 10 rock bands.'''
+
+select 	artist.artist_id, artist.name, count(artist.artist_id) as total_track
+from track
+join album on album.album_id = track.album_id
+join artist on artist.artist_id = album.artist_id
+join  genre on genre.genre_id = track.genre_id
+where genre.name like 'Rock'
+group by artist.artist_id
+order by total_track desc
+limit 10
+
+
+3 'Return all the track names that have a song length longer than the average song length.
+Return the Name and Milliseconds for each track.
+Order by the song length with the longest songs listed first.'''
+
+ select name, milliseconds from track 
+ where milliseconds > (
+        Select avg(milliseconds) as avg_track_lenght from track)
+ order by milliseconds desc;
+ 
---
+++ Modrate level.sql
@@ -0,0 +1,46 @@
+--EASY QUESTION
+1 --Who is the senior most employee based on job title?
+Select * from employee;
+
+select * from employee
+order by levels desc
+limit 1;
+
+
+2 --Which countries have the most Invoices?
+select * from invoice;
+
+select count(*) as most_invoice, billing_country from invoice 
+group by billing_country
+order by most_invoice desc;
+
+
+3 --What are top 3 values of total invoice?
+
+select * from invoice
+order by total desc
+limit 3;
+
+
+
+4 -'''Which city has the best customers? 
+  We would like to throw a promotional Music Festival in the city we made the most money.
+  Write a query that returns one city that has the highest sum of invoice totals.
+  Return both the city name & sum of all invoice totals.'''
+  
+select sum(total) as invoice_total, billing_city from invoice
+group by billing_city
+order by invoice_total desc;
+
+
+
+5 -' Who is the best customer?
+The customer who has spent the most money will be declared the best customer. 
+Write a query that returns the person who has spent the most money.'''  
+
+select c.customer_id, c.first_name, c.last_name , sum(i.total) as total
+from customer c
+join invoice i on c.customer_id = i.customer_id
+group by c.customer_id
+order by total desc
+limit 1; 
